9145b234fe2750f835208130a254977e846e696b
Bianca Pelcastre Varela <bianka@iMac-de-Bianca.local>
2023-03-23 11:38:16 -0600

    useState para el nombre minudev

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,28 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import { TwiterFoolowCard } from './TwiterFoolowCard'
 import { Tittle } from './Tittle'
 
 export function App () {
     const formatUserName = (userName) => `@${userName}`
+    const [name, setName]= useState('midudev')
     return(
     <section className='App'>
         <Tittle title={"Follow Card"}/>
-        <TwiterFoolowCard formatUserName={formatUserName}  isFololowing userName="midudev">
+        <TwiterFoolowCard formatUserName={formatUserName}   userName={name}>
         Minu Dev
         </TwiterFoolowCard>
 
-        <TwiterFoolowCard formatUserName={formatUserName}  isFololowing= {false} userName="pheralb">
+        <TwiterFoolowCard formatUserName={formatUserName}  userName="pheralb">
         Pablo Hernandez
         </TwiterFoolowCard> 
 
-        <TwiterFoolowCard formatUserName={formatUserName}  isFololowing userName="elonmusk">
+        <TwiterFoolowCard formatUserName={formatUserName}  userName="elonmusk">
         Elon Musk
         </TwiterFoolowCard> 
 
-        <TwiterFoolowCard formatUserName={formatUserName}  isFololowing userName="vxnder">
+        <TwiterFoolowCard formatUserName={formatUserName}  userName="vxnder">
         Vanderhart
         </TwiterFoolowCard> 
+        
+        <button onClick={()=> setName('pedromichel')}>
+            Cambio el nombre
+        </button>
 
     </section>
     
--- projects/00-hola-mundo/src/TwiterFoolowCard.jsx
+++ projects/00-hola-mundo/src/TwiterFoolowCard.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react"
 
-
 export function TwiterFoolowCard ({formatUserName,userName, children}) {
     const[isFololowing, setIsFollowing] = useState(false)
 
@@ -13,27 +12,24 @@ export function TwiterFoolowCard ({formatUserName,userName, children}) {
      setIsFollowing(!isFololowing) //hocs. de trua fals o de flas a true
     }
         
-
-
     return(
         <article className='tw-followCard'>
-          <header className='tw-followCard-header'>
-              <img 
+            <header className='tw-followCard-header'>
+                <img 
                  className='tw-followCard-avatar'
                  alt='El avatar del usuario'
                  src={`https://unavatar.io/${userName}`} />
                     
-            
-              <div className='tw-followCard-info'>
-                  <strong>{children}</strong>
-                  <span className='tw-followCard-infoUserName'>{formatUserName(userName)}</span>
-              </div>
-          </header>
-          <aside>
-              <button className={buttonClassName} onClick={handleState}>
+                 <div className='tw-followCard-info'>
+                    <strong>{children}</strong>
+                    <span className='tw-followCard-infoUserName'>{formatUserName(userName)}</span>
+                </div>
+            </header>
+            <aside>
+                <button className={buttonClassName} onClick={handleState}>
                   {text}
-              </button>
-          </aside>
+                </button>
+            </aside>
         </article>
       )
 }
