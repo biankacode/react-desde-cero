adc026a0c209a189db568cfb3231b4168aa6512c
Bianca Pelcastre Varela <bianka@iMac-de-Bianca.local>
2023-05-02 19:42:51 -0600

    proyecto 3

--- projects/02-tic-tac-toe/src/components/Square.jsx
+++ projects/02-tic-tac-toe/src/components/Square.jsx
@@ -1,17 +1,14 @@
 // Este es el cuadrado del tablero,children: lo que tiene adentro el tablero
-//updateBoard:cuando agamos clic se actualiza el tablero
+/* //updateBoard:cuando agamos clic se actualiza el tablero */
 export const Square = ({children, isSelected, updateBoard, index}) => {
-    const className = `square ${isSelected ? 'is-selected' : ''}`
-    /* console.log(className); */
-  
-    const handelClick = () => {
-      updateBoard(index)
-    } 
-    
-    
-   return (
+  const className = `square ${isSelected ? 'is-selected' : ''}`
+  /* console.log(className); */
+  const handelClick = () => {
+    updateBoard(index)
+  }
+  return (
     <div onClick={handelClick} className={className}>
       {children}
     </div>
-   )
-  } 
+  )
+}
