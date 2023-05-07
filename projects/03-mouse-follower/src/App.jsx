83f59da6f8f5687a73e8c5d4430e55a523135ce6
Bianca Pelcastre Varela <bianka@iMac-de-Bianca.local>
2023-05-06 18:28:04 -0600

    posición del puntero

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -2,9 +2,18 @@ import { useEffect, useState } from "react"
 
 function App() {
   const [enabled, setEnabled] = useState(false)
+  const [activar, setActivar] = useState(false)
   useEffect(() => {
-    console.log('efecto')
-  })
+    console.log('efecto',{enabled})
+    const handleMove = (event) =>{ // la function recive el evento
+      const {clientX, clientY} = event //plano carteciano del puntero
+      console.log(handleMove, {clientX,clientY});//posicion exacta del puntero
+    }
+    window.addEventListener('pointermove', handleMove)//cuando se mueva el puntero ejecutamos la fuccion handelMove
+  }, [enabled])// activa y desactiva cuando segir el puntero
+
+ 
+
   return (
     <main>
      <div style={{
@@ -21,8 +30,15 @@ function App() {
       /*   transform: `translate(${position.x}px, ${position.y}px)` */
      }}
      />
-      <button onClick={()=> setEnabled(!enabled)}>
-      {enabled? 'Desactivar' : 'Activar'} segir puntero </button>
+      <button onClick={() => setEnabled(!enabled)}>
+      {enabled? 'Desactivar' : 'Activar'} segir puntero 
+      </button>
+      <section>
+        <button onClick={() => setActivar(!activar)}>
+          {activar? 'Abrir': 'Cerrar'} 
+        </button>
+      </section>
+     
     </main>
   )
 }
