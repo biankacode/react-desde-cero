70f6741a7f9df77830e7bb268649a8b798147269
Bianca Pelcastre Varela <bianka@iMac-de-Bianca.local>
2023-05-08 13:27:14 -0600

    FolowMuse

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 
-function App() {
+const FolowMuse = () =>{
   const [enabled, setEnabled] = useState(false)
   const [activar, setActivar] = useState(false)
   const [position, setPosition] = useState({x:0, y:0}) // aqui guardamos la posicion del puntero
@@ -13,6 +13,9 @@ function App() {
       console.log(handleMove, {clientX,clientY});//posicion exacta del puntero
       setPosition({x:clientX, y:clientY}) 
     }
+    // Cleanup
+    // Cuando el componente se desmonta 
+    //Cundo cambian las dependencias antes de ejecutar
 
     if (enabled) { //solo activar si el anbled es true
       window.addEventListener('pointermove', handleMove)//cuando se mueva el puntero ejecutamos la fuccion handelMove
@@ -22,33 +25,38 @@ function App() {
     }
   }, [enabled])// activa y desactiva cuando segir el puntero
 
+  return(
+    <>
+    <div style={{
+          position: 'absolute',
+          backgroundColor: '#09f',
+          border: '1px solid #fff',
+          borderRadius: '50%',
+          opacity: 0.8,
+          pointerEvents: 'none',
+          left: -25,
+          top: -25,
+          width: 50,
+          height: 50,
+          transform: `translate(${position.x}px, ${position.y}px)`
+      }}
+      />
+        <button onClick={() => setEnabled(!enabled)}>
+        {enabled? 'Desactivar' : 'Activar'} segir puntero 
+        </button>
+        <section>
+            <button onClick={() => setActivar(!activar)}>
+              {activar? 'Abrir': 'Cerrar'} 
+            </button>
+        </section>
+   </>     
+  )
+}
  
-
+function App() {
   return (
     <main>
-     <div style={{
-        position: 'absolute',
-        backgroundColor: '#09f',
-        border: '1px solid #fff',
-        borderRadius: '50%',
-        opacity: 0.8,
-        pointerEvents: 'none',
-        left: -25,
-        top: -25,
-        width: 50,
-        height: 50,
-        transform: `translate(${position.x}px, ${position.y}px)`
-     }}
-     />
-      <button onClick={() => setEnabled(!enabled)}>
-      {enabled? 'Desactivar' : 'Activar'} segir puntero 
-      </button>
-      <section>
-        <button onClick={() => setActivar(!activar)}>
-          {activar? 'Abrir': 'Cerrar'} 
-        </button>
-      </section>
-     
+      <FolowMuse/>
     </main>
   )
 }
