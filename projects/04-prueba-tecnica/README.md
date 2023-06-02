8be8f06bf30adbfd5ba062d873d3ac0d408aada8
Bianca Pelcastre Varela <bianka@iMac-de-Bianca.local>
2023-06-02 12:32:00 -0600

    Fetching de datos con useEffect

--- projects/04-prueba-tecnica/README.md
+++ projects/04-prueba-tecnica/README.md
@@ -5,6 +5,7 @@ APIs:
 - Facts Random: https://catfact.ninja/fact
 
 - Imagen random: https://cataas.com/cat/says/hello
+- Endopoint para usar: `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
 
 * Recupera un hecho aleatorio de gatos de la primera API
 
--- projects/04-prueba-tecnica/src/App/App.jsx
+++ projects/04-prueba-tecnica/src/App/App.jsx
@@ -1,10 +1,16 @@
 import{useState, useEffect} from 'react'
+const CAT_ENPOINR_RANDOM_FACT = 'https://catfact.ninja/fact'
+//const CAT_ENPOINT_IMG  = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
+
 
 export function App () {
-    const [fact, setFact] = useState('Lorep ipsum cat what for ever')
+
+    const [fact, setFact] = useState('Lorem ipsum cat what for ever')
 
     useEffect(() => {
-
+     fetch(CAT_ENPOINR_RANDOM_FACT)
+          .then(resp=>resp.json())
+          .then(data => setFact(data.fact))
     },[])
    return(
     <main>
