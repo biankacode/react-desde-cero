4fb3582ec16dfde0a9686831b36340282c093aaa
Bianca Pelcastre Varela <bianka@iMac-de-Bianca.local>
2023-06-02 12:57:31 -0600

    Renderizado condicional

--- projects/04-prueba-tecnica/src/App/App.jsx
+++ projects/04-prueba-tecnica/src/App/App.jsx
@@ -1,15 +1,21 @@
 import{useState, useEffect} from 'react'
+const CAT_ENPOINR_RANDOM_FACT = 'https://catfact.ninja/fact'
+//const CAT_ENPOINT_IMG  = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
+
 
 export function App () {
-    const [fact, setFact] = useState('Lorep ipsum cat what for ever')
+
+    const [fact, setFact] = useState()
 
     useEffect(() => {
-
+     fetch(CAT_ENPOINR_RANDOM_FACT)
+          .then(resp=>resp.json())
+          .then(data => setFact(data.fact))
     },[])
    return(
     <main>
          <h1>App de gatitos</h1>
-         <p>{fact}</p>
+         {fact && <p>{fact}</p>} {/* Renderizado condicional  */}
     </main>
    
    )
